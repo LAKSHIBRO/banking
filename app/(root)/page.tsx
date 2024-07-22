a897e26b56e723996f20ff7601da48e33cc5c1f6
Lakshitha <notlakshitha@gmail.com>
2024-07-22 21:56:00 +0530

    feat: Add pages for MyBanks, Payment Transfer, and Transaction History
    - Add new pages for MyBanks, Payment Transfer, and Transaction History
    - Each page contains a simple component with a placeholder text
    - These pages will serve as the foundation for future development

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = {
+    firstName: "Deshan",
+    lastName: "Lakshitha",
+    email: "notakshitha@gmail.com",
+  };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "guest"}
+            subtext="Access and manage your account and transactions efficiantly."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1504.4}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 558.5 }, { currentBalance: 188.7 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
